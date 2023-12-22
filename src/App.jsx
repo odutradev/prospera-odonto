aec2a86f5ccd4aabe6c1cb34f046ec5ce7d9dd36
Dutra PC <joao.vitornl@gmail.com>
2023-12-22 12:36:19 -0300

    :wrench: chore: Configure vite features and config local file

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { ThemeProvider } from 'styled-components';
 import Router from './routes';
 import {theme} from './styles/theme'
 import GlobalStyle from './styles/globalStyles'
+import config from './assets/config';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-
+      <title>{config?.pageTitle}</title>
       <GlobalStyle />
       <Router />
     </ThemeProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
+export default defineConfig(({ command, mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: '8080'
+    },
+    build:{
+      outDir: 'build'
+    }
   }
 })
