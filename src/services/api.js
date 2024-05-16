aa5a99fa07f5c7920244692de7f17cca383c312b
dutra-pc <joao.vitornl@gmail.com>
2024-05-15 22:59:40 -0300

    :construction: Update api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 var token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: "https://830656a8-78af-4cdf-935c-49d2c12051db-00-38ec9n63j8jfl.worf.replit.dev/v3/",
+  baseURL: "https://api.prosperaodonto.litetechnology.space/v1/",
   headers: {
     'Content-Type': 'application/json',
     'authorization': token
