ad6cba8f5a5310fe69438d013f004b3730662b82
dutra-pc <joao.vitornl@gmail.com>
2024-05-17 17:14:38 -0300

    :construction: Remove auth error

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
-import { Container, Box } from './styles';
+const NotFoundPage = () => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        p: 3,
+      }}
+    >
+      <Typography variant="h1" sx={{ fontSize: '6rem', mb: 2 }}>
+        4<ErrorOutlineIcon sx={{ fontSize: '5rem' }} />4
+      </Typography>
+      <Typography variant="h5" sx={{ mb: 3 }}>
+        Oops! Essa pagina nao existe.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<HomeIcon />}
+        onClick={() => window.location.href = '/'}
+        sx={{ textTransform: 'none' }}
+      >
+        Volte para pagina inicial
+      </Button>
+    </Box>
+  );
+};
 
-const Error = () => {
-
-    return (
-       <>
-       teste
-       </>
-    )
-}
-
-export default Error
+export default NotFoundPage;
