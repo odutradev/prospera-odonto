0632d9aecb565d4fcd79d57fea3548b9d3312268
João Dutra <79696304+odutradev@users.noreply.github.com>
2025-07-09 11:09:08 -0300

    feat: Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 var token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: true ? "https://api.prosperaodonto.pro/v1/" : "http://localhost:1000/v1/",
+  baseURL: true ? "https://api.prosperaodonto.com.br/v1/" : "http://localhost:1000/v1/",
   headers: {
     'Content-Type': 'application/json',
     'authorization': token
